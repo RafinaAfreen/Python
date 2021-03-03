05f21e6672f5f45737e0a75adc00c35434b94781
RAfinaAfreen <rafinaafreen@gmail.com>
2021-03-03 20:45:49 +0600

    modules of python

---
+++ PartTwo/Module/myprogram.py
@@ -0,0 +1,8 @@
+import mymodule
+#2import mymodule as mm
+#mm.func_in_module
+#3from mymodule import func_in_module
+#func_in_module
+#4from mymodule import *
+#func_in_module
+mymodule.func_in_module()
